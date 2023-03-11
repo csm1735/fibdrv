6df84c2505f0702edd4a4b4da04763ede06b8e8f
csm1735 <asd148148@gmail.com>
2023-03-11 15:48:50 +0800

    Implement fast doubling

--- fibdrv.c
+++ fibdrv.c
@@ -24,19 +24,42 @@ static struct cdev *fib_cdev;
 static struct class *fib_class;
 static DEFINE_MUTEX(fib_mutex);
 
+// static long long fib_sequence(long long k)
+// {
+//     /* FIXME: C99 variable-length array (VLA) is not allowed in Linux kernel.
+//     */ long long f[k + 2];
+
+//     f[0] = 0;
+//     f[1] = 1;
+
+//     for (int i = 2; i <= k; i++) {
+//         f[i] = f[i - 1] + f[i - 2];
+//     }
+
+//     return f[k];
+// }
+
 static long long fib_sequence(long long k)
 {
-    /* FIXME: C99 variable-length array (VLA) is not allowed in Linux kernel. */
-    long long f[k + 2];
+    if (k <= 2)
+        return !!k;
 
-    f[0] = 0;
-    f[1] = 1;
+    uint8_t count = 63 - __builtin_clzll(k);
+    uint64_t n0 = 1, n1 = 1;
 
-    for (int i = 2; i <= k; i++) {
-        f[i] = f[i - 1] + f[i - 2];
+    for (uint64_t i = count; i-- > 0;) {
+        uint64_t fib_2n0 = n0 * ((n1 << 1) - n0);
+        uint64_t fib_2n1 = n0 * n0 + n1 * n1;
+
+        if (k & (1UL << i)) {
+            n0 = fib_2n1;
+            n1 = fib_2n0 + fib_2n1;
+        } else {
+            n0 = fib_2n0;
+            n1 = fib_2n1;
+        }
     }
-
-    return f[k];
+    return n0;
 }
 
 static int fib_open(struct inode *inode, struct file *file)
